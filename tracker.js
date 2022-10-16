c7f6da3940113900eda8d617d550820a89c70f1d
oussama <romdhane.oussema1@gmail.com>
2022-10-16 01:19:35 +0100

    improve

--- tracker.js
+++ tracker.js
@@ -1,5 +1,63 @@
-console.log("hello world");
+let mouseData = [];
+let isInactiveData = [];
 
-onmousemove = function (e) {
-  console.log("mouse location:", e.clientX, e.clientY);
+let apiKey = localStorage.getItem("upsideApiKey");
+let user = localStorage.getItem("upsideUser");
+
+let currentPosition = {
+  x: 0,
+  y: 0,
+  time: new Date().toISOString(),
+  url: window.location.href,
 };
+
+let isInactive = {
+  stoppedTime: new Date().toISOString(),
+  activatedTime: new Date().toISOString(),
+  duration: 0,
+};
+
+document.addEventListener("visibilitychange", () => {
+  const now = new Date();
+  console.log(now);
+  if (document.hidden) {
+    isInactive.stoppedTime = now.toISOString();
+  } else {
+    isInactive.activatedTime = now.toISOString();
+    isInactive.duration =
+      (new Date(isInactive.activatedTime) - new Date(isInactive.stoppedTime)) /
+      1000;
+    isInactiveData.push(isInactive);
+  }
+});
+
+document.addEventListener("mousemove", (e) => {
+  currentPosition.x = e.clientX;
+  currentPosition.y = e.clientY;
+});
+
+setInterval(() => {
+  const newPosition = Object.assign(
+    { time: new Date().toISOString(), url: window.location.href },
+    { x: currentPosition.x, y: currentPosition.y }
+  );
+
+  if (mouseData.length < 1) {
+    return mouseData.push(newPosition);
+  }
+
+  mouseData.push(newPosition);
+}, 1000);
+
+setInterval(() => {
+  fetch("http://localhost:5000/data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": apiKey,
+    },
+    body: JSON.stringify({ mouseData, isInactiveData, user }),
+  });
+  mouseData = [];
+  isInactiveData = [];
+}, 5000);
